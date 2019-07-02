cc415145da6425f22ddfb8f7d90fd3658d850c49
Chrustkiran Sathiyananthadevar <christkiran.15@cse.mrt.ac.lk>
2019-07-02 12:14:50 +0530

    Update Buffer.h
    updated header

--- threadpool/Buffer.h
+++ threadpool/Buffer.h
@@ -1,33 +1,14 @@
-//
-// Created by chrustkiran on 6/12/19.
-//
-
-/*
-#ifndef STREAMPROCESSORLLVM_TP_BUFFER_H
-#define STREAMPROCESSORLLVM_TP_BUFFER_H
-
-#include <iostream>
-#include <queue>
-
-using namespace std;
-
-template <class T>
-class Buffer {
-
-private:
-    queue<T*> *eventQueue;
-
-public:
-    void pushToBuffer();
-    void getFromBuffer();
-
-
-
-
-};
-
-
-#endif //STREAMPROCESSORLLVM_TP_BUFFER_H
+/**
+Copyright 2019 Siddhi-LLVM Team
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 
